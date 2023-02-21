72a3d7e063dd8be91eef6e443293e2f675dbe732
Chatchai Luangmanee <93616024+chatchai-outreach@users.noreply.github.com>
2023-02-20 19:28:30 -0800

    feat: support canary deployment

--- templates/main_test.go
+++ templates/main_test.go
@@ -36,6 +36,22 @@ func TestRenderDeploymentJsonnet(t *testing.T) {
 	st.Run(true)
 }
 
+func TestRenderDeploymentJsonnet_Canary(t *testing.T) {
+	st := stenciltest.New(t, "deployments/appname/app.jsonnet.tpl", libaryTmpls...)
+	st.Args(map[string]interface{}{
+		"reportingTeam": "test:team",
+		"deployment": map[string]interface{}{
+			"strategy": "canary",
+		},
+		"service": true,
+		"serviceActivities": []interface{}{
+			"http",
+		},
+		"slack": "hello",
+	})
+	st.Run(true)
+}
+
 func TestRenderDeploymentOverride(t *testing.T) {
 	st := stenciltest.New(t, "deployments/appname/app.override.jsonnet.tpl", libaryTmpls...)
 	st.Run(true)
